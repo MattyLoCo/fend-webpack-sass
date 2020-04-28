e03f184e51c75a636d4767a64ddde2782af2a824
Matthew Lopez <lopezmatt6891@gmail.com>
2020-04-28 11:46:03 -0600

    Debug api fetch functionality

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -7,10 +7,17 @@ function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    fetch('https://api.openweathermap.org/data/2.5/weather?zip=80909&units=imperial&appid=91b0b55d837ff53bcd4a0c367014bd60')
     .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+    .then((res) => {
+        console.log(`API response is ${typeof res}`);
+        let data = res;
+        let temperature = data.main.temp;
+        return temperature;
+    })
+    .then(function(temperature) {
+        document.getElementById('results').innerHTML = `Good day, captain ${formText}! The \
+        current temperature is a balmy ${temperature} degrees.`
     })
 }
 
